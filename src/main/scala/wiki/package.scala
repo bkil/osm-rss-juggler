e199102307f7cb7c3079417cfa5e2959fcb6e774
bkil <b.K.il.h.u+tigbuh@gmail.com>
2020-12-07 22:09:44 +0100

    wiki: fixup to make it compile

--- src/main/scala/wiki/package.scala
+++ src/main/scala/wiki/package.scala
@@ -7,7 +7,7 @@ package object wiki {
     val xml1 = new URL("https://wiki.openstreetmap.org/w/api.php?hidebots=1&hideWikibase=1&urlversion=2&days=1&limit=5&target=Hungary/Tal%C3%A1lkoz%C3%B3k&action=feedrecentchanges&feedformat=rss")
     val doc1 = WikiParser.parse(Html.fetch(xml1))
 
-    val xml2 = new URL("https://wiki.openstreetmap.org/w/api.php?hidebots=1&hideWikibase=1&urlversion=2&days=1&limit=5&target=Hungary&action=feedrecentchanges&feedformat=rss"))
+    val xml2 = new URL("https://wiki.openstreetmap.org/w/api.php?hidebots=1&hideWikibase=1&urlversion=2&days=1&limit=5&target=Hungary&action=feedrecentchanges&feedformat=rss")
     val doc2 = WikiParser.parse(Html.fetch(xml2))
 
     val doc = doc1.distinctConcat(doc2)
