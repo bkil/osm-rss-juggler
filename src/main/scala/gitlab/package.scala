b442cd505a29e7b6d1341ab98ca07221a8f96000
bkil <b.K.il.h.u+tigbuh@gmail.com>
2020-12-16 03:02:31 +0100

    gitlab: produce a feed that has link redirects to avoid unfurling

--- src/main/scala/gitlab/package.scala
+++ src/main/scala/gitlab/package.scala
@@ -1,6 +1,6 @@
 import java.net.URL
 
-import xml.Html
+import xml.{Html, Rss}
 
 package object gitlab {
   def run(): Unit = {
@@ -8,5 +8,9 @@ package object gitlab {
     val doc = GitlabParser.parse(Html.fetch(url))
     println(doc.items.length, doc.title)
     doc.save("gitlab-humor.xml")
+
+    val prefix = "http://bkil.hu/kludge/redirect.html?url="
+    val redirected = doc.transformItems(Rss.Item.prefixLink(prefix))
+    redirected.save("gitlab-humor-redirect.xml")
   }
 }
