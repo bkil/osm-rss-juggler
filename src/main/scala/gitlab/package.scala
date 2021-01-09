6ff661c8967aeed4d68a45c33dc456e37084d296
bkil <b.K.il.h.u+tigbuh@gmail.com>
2021-01-09 04:10:27 +0100

    gitlab/package: summarize piclayer commits too

--- src/main/scala/gitlab/package.scala
+++ src/main/scala/gitlab/package.scala
@@ -4,6 +4,10 @@ import xml.{Html, Rss}
 
 package object gitlab {
   def run(): Unit = {
+    val piclayerUrl = new URL("https://gitlab.com/bkil/osm-piclayer-cal/-/commits/master?format=atom")
+    val piclayerDoc = GitlabParser.parse(Html.fetch(piclayerUrl))
+    piclayerDoc.save("gitlab-piclayer.xml")
+
     val url = new URL("https://gitlab.com/bkil/humor/-/commits/master?format=atom")
     val doc = GitlabParser.parse(Html.fetch(url))
     println(doc.items.length, doc.title)
